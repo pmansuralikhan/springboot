68a19e824f89b7d0ea31144859f5291fd3f8882b
pmansuralikhan <pmansuralikhan@gmail.com>
2019-03-14 16:52:59 +0530

    Relationship between Topic and Course - o

--- src/main/java/com/oracle/practice/springboot/course/CourseRepository.java
+++ src/main/java/com/oracle/practice/springboot/course/CourseRepository.java
@@ -1,7 +1,9 @@
 package com.oracle.practice.springboot.course;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface CourseRepository extends CrudRepository<Course, String> {
-
+	List<Course> findByTopicId(String topicId);
 }
